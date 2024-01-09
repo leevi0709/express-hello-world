3fd15497fc302bd5dce9d99769beb2809d56772f
xduo <xduo@noreply.gitcode.com>
2024-01-09 17:37:08 +0800

    测试壁纸

--- public/js/main.js
+++ public/js/main.js
@@ -50,26 +50,33 @@ $(document).ready(function () {
 	 * 改用 JsonBird：https://bird.ioliu.cn/
 	 * 
 	 */
-	var url = 'https://api.heylie.cn/api/img?xq=wz';
+	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
 	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
 	var index = sessionStorage.getItem("index");
 	var $panel = $('#panel');
 	if (imgUrls == null) {
 		imgUrls = new Array();
 		index = 0;
-$.get(url, function (e) {
-    var imgUrl = 6.png;
-    var url = https://cdn.heylie.cn/wz/;
-    $panel.css("background", "url('" + url + "') center center no-repeat #666");
-    $panel.css("background-size", "cover");
-});
+		$.get(url, function (result) {
+			images = result.images;
+			for (let i = 0; i < images.length; i++) {
+				const item = images[i];
+				imgUrls.push(item.url);
+			}
+			var imgUrl = imgUrls[index];
+			var url = "https://www.bing.com" + imgUrl;
+			$panel.css("background", "url('" + url + "') center center no-repeat #666");
+			$panel.css("background-size", "cover");
+			sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
+			sessionStorage.setItem("index", index);
+		});
 	} else {
 		if (index == 7)
 			index = 0;
 		else
 			index++;
-var imgUrl = 6.png;
-var url = https://cdn.heylie.cn/wz/;
+		var imgUrl = imgUrls[index];
+		var url = "https://www.bing.com" + imgUrl;
 		$panel.css("background", "url('" + url + "') center center no-repeat #666");
 		$panel.css("background-size", "cover");
 		sessionStorage.setItem("index", index);
