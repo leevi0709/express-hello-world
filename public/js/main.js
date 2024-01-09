73e6f3e74b91a89de29766a11469b12ae139d67c
xduo <xduo@noreply.gitcode.com>
2024-01-09 17:48:21 +0800

    12

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
 	 * 改用 JsonBird：https://bird.ioliu.cn/
 	 * 
 	 */
-	var url = 'https://api.pearktrue.cn/api/imageaccelerate/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
+	var url = 'https://bird.ioliu.cn/v2/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
 	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
 	var index = sessionStorage.getItem("index");
 	var $panel = $('#panel');
